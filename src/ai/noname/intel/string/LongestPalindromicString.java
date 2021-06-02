043a315a180afa924df36588175ba23c65abc398
spatasha <sidhm12@gmail.com>
2021-06-02 08:55:53 -0700

    CTCI Questions- String and Arrays

--- src/ai/noname/intel/string/LongestPalindromicString.java
+++ src/ai/noname/intel/string/LongestPalindromicString.java
@@ -6,9 +6,9 @@ public class LongestPalindromicString {
         String result = "";
         while (startIdx < s.length() - 1) {
             int i = startIdx - 1, j = startIdx + 1;
-            while (i >= 0 && i < s.length() - 1 - j) {
-                String subString = s.substring(i, j);
-                if (subString.charAt(i) == subString.charAt(j)) {
+            while (i >= 0 ) {
+                String subString = s.substring(i, j+1);
+                if (s.charAt(i) == s.charAt(j)) {
                     i--;
                     j++;
                 } else {
@@ -25,6 +25,6 @@ public class LongestPalindromicString {
 
     public static void main(String[] args) {
         LongestPalindromicString longestPalindromicString=new LongestPalindromicString();
-        System.out.println("longestPalindromicString::"+longestPalindromicString.longestPalindrome("ceabcbafqrelovuolexyz"));
+        System.out.println("longestPalindromicString::"+longestPalindromicString.longestPalindrome("babad"));
     }
 }
